320a5de63bb0ea9b8eda91064704c05df197b811
Walbre <brewalguyon@gmail.com>
2023-03-11 11:14:47 +0100

    commit

--- monkey_prog.py
+++ monkey_prog.py
@@ -7,4 +7,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
 print(s.recv(1024).decode("utf-8"))
-prin(s.recv(1024).decode("utf-8"))
+print(s.recv(1024).decode("utf-8"))
+
+s.send("0")
+
+for i in range(3):
+    s.recv(1024)
+
+print(s.recv(1024))
